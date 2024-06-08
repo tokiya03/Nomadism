88cc55673fea86f8e3b6bdbcd49a1133dddd6039
Hiroyuki Tokiya <163287178+tokiya03@users.noreply.github.com>
2024-06-08 12:06:06 +0000

    fixed migrate

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Nomadism
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
 
     # Configuration for the application, engines, and railties goes here.
     #
