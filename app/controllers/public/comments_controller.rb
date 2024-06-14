d92678d924ccf811d7d7cf537d73224d6e669378
Hiroyuki Tokiya <163287178+tokiya03@users.noreply.github.com>
2024-06-14 22:34:03 +0900

    add flash messege and comment count asynchronous

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -6,13 +6,23 @@ class Public::CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.new(comment_params)
     @comment.post_id = @post.id
-    @comment.save
+    if @comment.comment.blank?
+      flash.now[:info] = 'コメントを入力してください。'
+    elsif @comment.save
+      flash.now[:success] = 'コメントを投稿しました。'
+    else
+      flash.now[:danger] = 'コメントの投稿に失敗しました。'
+    end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
-    @comment.destroy
+    if @comment.destroy
+      flash.now[:success] = 'コメントを削除しました。'
+    else
+      flash.now[:danger] = 'コメントの削除に失敗しました。'
+    end
   end
 
   # ストロングパラメータ
