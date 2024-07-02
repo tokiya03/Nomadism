dea4718fbaa5b51d56d2abe211c6c07f9716355f
Hiroyuki Tokiya <163287178+tokiya03@users.noreply.github.com>
2024-07-02 22:17:18 +0900

    fixed api linkage setting

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,135 +11,135 @@ puts "seedの実行を開始しました"
 
 ## テストデータ用
 # デフォルトのユーザーを作成（10ユーザー）
-Justin = User.find_or_create_by!(email: "justin@example.jp") do |user|
+justin = User.find_or_create_by!(email: "justin@example.jp") do |user|
   user.name = "ジャスティン"
   user.password = "password"
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
 end
 
-Sara = User.find_or_create_by!(email: "Sara@example.jp") do |user|
+sara = User.find_or_create_by!(email: "Sara@example.jp") do |user|
   user.name = "サラ"
   user.password = "password"
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")
 end
 
-Lucas = User.find_or_create_by!(email: "lucas@example.jp") do |user|
+lucas = User.find_or_create_by!(email: "lucas@example.jp") do |user|
   user.name = "ルーカス"
   user.password = "password"
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
 end
 
-Selena = User.find_or_create_by!(email: "selena@example.jp") do |user|
+selena = User.find_or_create_by!(email: "selena@example.jp") do |user|
   user.name = "セレナ"
   user.password = "password"
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user4.jpg"), filename:"sample-user4.jpg")
 end
 
-Tom = User.find_or_create_by!(email: "tom@example.jp") do |user|
+tom = User.find_or_create_by!(email: "tom@example.jp") do |user|
   user.name = "トム"
   user.password = "password"
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user5.jpg"), filename:"sample-user5.jpg")
 end
 
-Risa = User.find_or_create_by!(email: "risa@example.jp") do |user|
+risa = User.find_or_create_by!(email: "risa@example.jp") do |user|
   user.name = "リサ"
   user.password = "password"
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user6.jpg"), filename:"sample-user6.jpg")
 end
 
-James = User.find_or_create_by!(email: "james@example.jp") do |user|
+james = User.find_or_create_by!(email: "james@example.jp") do |user|
   user.name = "ジェームズ"
   user.password = "password"
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user7.jpg"), filename:"sample-user7.jpg")
 end
 
-Alice = User.find_or_create_by!(email: "alice@example.jp") do |user|
+alice = User.find_or_create_by!(email: "alice@example.jp") do |user|
   user.name = "アリス"
   user.password = "password"
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user8.jpg"), filename:"sample-user8.jpg")
 end
 
-Eito = User.find_or_create_by!(email: "eito@example.jp") do |user|
+eito = User.find_or_create_by!(email: "eito@example.jp") do |user|
   user.name = "瑛人"
   user.password = "password"
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user9.jpg"), filename:"sample-user9.jpg")
 end
 
-Asami = User.find_or_create_by!(email: "asami@example.jp") do |user|
+asami = User.find_or_create_by!(email: "asami@example.jp") do |user|
   user.name = "麻美"
   user.password = "password"
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user10.jpg"), filename:"sample-user10.jpg")
 end
 
-# デフォルトの投稿を作成（10件）
+# デフォルトの投稿を作成（10件)
 Post.find_or_create_by!(name: "テストCafe") do |post|
   post.caption = "充電可能なコンセントと無料Wi-Fiが備わっているのと、カフェラテが美味しかったので、とても作業が捗りました。"
   post.score = (Language.get_data(post.caption) + 1) * 50
   post.address = "東京都世田谷区"
-  post.user = Justin
+  post.user = justin
 end
 
 Post.find_or_create_by!(name: "緑Cafe") do |post|
   post.caption = "観葉植物に囲まれていて、いい感じの曲が流れていて、とても集中して学習を行うことができました。"
   post.score = (Language.get_data(post.caption) + 1) * 50
   post.address = "東京都杉並区"
-  post.user = Sara
+  post.user = sara
 end
 
 Post.find_or_create_by!(name: "青びん珈琲") do |post|
   post.caption = "無料のWi-Fiが時間制限なしで使えるため、3時間も滞在して作業を行なってしまいました。"
   post.score = (Language.get_data(post.caption) + 1) * 50
   post.address = "大阪府堺市"
-  post.user = Lucas
+  post.user = lucas
 end
 
 Post.find_or_create_by!(name: "緑の丘") do |post|
   post.caption = "晴れた日には市内を一望できるので、気分よく学習を行うことができる場所でした。"
   post.score = (Language.get_data(post.caption) + 1) * 50
   post.address = "富山県富山市"
-  post.user = Selena
+  post.user = selena
 end
 
 Post.find_or_create_by!(name: "Amazing Summer Cafe") do |post|
   post.caption = "店内がとても明るかったので、気分よく作業を行うことができる場所でした。"
   post.score = (Language.get_data(post.caption) + 1) * 50
   post.address = "北海道釧路市"
-  post.user = Tom
+  post.user = tom
 end
 
 Post.find_or_create_by!(name: "庭Cafe") do |post|
   post.caption = "綺麗な庭園を眺めながら作業ができたので、とても過ごしやすかったです。"
   post.score = (Language.get_data(post.caption) + 1) * 50
   post.address = "宮城県仙台市"
-  post.user = Risa
+  post.user = risa
 end
 
 Post.find_or_create_by!(name: "ノマドWork") do |post|
   post.caption = "時間単位で料金が発生してしまいますが、コンセントと無料Wi-Fiがあり、リモート会議ができる個室などがあって、1日中作業ができるコアワーキングスペースです。"
   post.score = (Language.get_data(post.caption) + 1) * 50
   post.address = "広島県廿日市"
-  post.user = James
+  post.user = james
 end
 
 Post.find_or_create_by!(name: "不思議の森コーヒー") do |post|
   post.caption = "広い店内でテーブル席と1人席には充電可能なコンセントがあって、無料で使えるWi-Fiがあるので、落ち着いて作業することができます。"
   post.score = (Language.get_data(post.caption) + 1) * 50
   post.address = "福岡県北九州市"
-  post.user = Alice
+  post.user = alice
 end
 
 Post.find_or_create_by!(name: "777 Lounge") do |post|
   post.caption = "晴れた日には開放感抜群の景色が広がるお店で、外を見るだけで気分転換になりました。"
   post.score = (Language.get_data(post.caption) + 1) * 50
   post.address = "愛媛県大洲市"
-  post.user = Eito
+  post.user = eito
 end
 
 Post.find_or_create_by!(name: "Galette") do |post|
   post.caption = "おしゃれなで広々とした店内で、とても過ごしやすいところでした。"
   post.score = (Language.get_data(post.caption) + 1) * 50
   post.address = "東京都渋谷区"
-  post.user = Asami
+  post.user = asami
 end
 
 # デフォルトのグループを作成（10件）
@@ -217,7 +217,7 @@ end
 admin_email = ENV["ADMIN_EMAIL"]
 admin_password = ENV["ADMIN_PASSWORD"]
 
-Admin = Admin.find_or_create_by!(email: admin_email) do |user|
+Admin.find_or_create_by!(email: admin_email) do |user|
   user.password = admin_password
 end
 
